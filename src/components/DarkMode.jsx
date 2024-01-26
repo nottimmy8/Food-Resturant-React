550331b39a9dc74d8a86d82ed53b6bb9d1b40166
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2024-01-26 04:09:28 -1100

    first commit

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,20 @@
+// import React from "react";
+
+// function DarkMode() {
+//   return (
+//     <div>
+//       <img
+//         src="src/assets/website/dark-mode-button.png"
+//         alt=""
+//         className="w-12 absolute right-0 z-10"
+//       />
+//       <img
+//         src="src/assets/website/light-mode-button.png"
+//         alt=""
+//         className="w-12 absolute right-0 z-10"
+//       />
+//     </div>
+//   );
+// }
+
+// export default DarkMode;
